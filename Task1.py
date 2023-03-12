2f1fad449ee064478470e83a0a288f4969070968
Анастасия Лазарева <lazyrkaa@gmail.com>
2023-03-12 11:19:28 +0600

    Add some beauty

--- Task1.py
+++ Task1.py
@@ -4,11 +4,16 @@ import matplotlib.pyplot as plt
 A0 = 2 # initial concentration of substance A
 k1 = 300 # rate constant of B formation
 k2 = 100 # rate constant of C formation
-t=np.linspace(0,0.02,40)
+t=np.linspace(0,0.03,50)
 B = k1/(k1+k2)*A0*(1-np.exp(t*(-k1-k2)))
 C = k2/(k1+k2)*A0*(1-np.exp(t*(-k1-k2)))
 A = A0*np.exp(t*(-k1-k2))
-plt.plot(t,B, color = 'b')
-plt.plot(t,C, color = 'r')
-plt.plot(t, A, color ='g')
+plt.title('Concentration of substances A, B and C')
+plt.ylabel('Concentration')
+plt.xlabel('time')
+plt.plot(t, B, color = 'b', label = 'substance B')
+plt.plot(t, C, color = 'r', label = 'substance C')
+plt.plot(t, A, color ='g', label = 'substance A')
+plt.legend(fontsize=10)
+
 plt.show()
