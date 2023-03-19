d00e6aef9cc1fea5ee73754d3df7ad969efb4e12
Анастасия Лазарева <lazyrkaa@gmail.com>
2023-03-19 11:10:44 +0600

    Add changes

--- functions/Scrabble game.py
+++ functions/Scrabble game.py
@@ -4,25 +4,29 @@
 # умещается ли слово на игровом поле, если задана позиция его первой буквы
 # как строка (например, 'G7'), переменная, указывающая расположение слова
 # по горизонтали или по вертикали, и само слово.
+import re
 pos, h_or_v, word = (i for i in input().split() if i != ' ') #это ГЕНЕРАТОР
 # [expr(variable) (функция) for variable(переменная) in iterable if condition(variable)]
-letters = 'ABCDE'
-numbers = '12345'
+letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
+numbers = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']
+nums = re.findall(r'\d+', pos) #ебать чета нашла в интернете, крутая штука
 def fit(pos,h_or_v, word):
     """determines if a word fits"""
     if h_or_v == 'h':
-        for i in pos:
-            if i.isdigit():
-                if (len(numbers.split('{}'.format(i))[1])+1) >= len(word):
-                    print('ok')
-                else:
-                    print('fuck')
+                for j in range(len(numbers)):
+                    if numbers[j] == nums[0]:
+                        if (len(numbers) - j) >= len(word):
+                            print('ok')
+                        else:
+                            print('fuck')
     if h_or_v == 'v':
         for i in pos:
             if not i.isdigit():
-                if (len(letters.split('{}'.format(i))[1])+1) >= len(word):
-                    print('ok')
-                else:
-                    print('fuck')
+                for j in range(len(letters)):
+                    if letters[j] == '{}'.format(i):
+                        if (len(letters) - j) >= len(word):
+                            print('ok')
+                        else:
+                            print('fuck')
 
 fit(pos, h_or_v, word)
